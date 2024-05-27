258d4b0f6be29053e73f43c092d24253723fb881
juanigm <juanigm99@gmail.com>
2024-05-27 12:02:04 -0300

    Terraform state configured for Azure Blob Storage

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-tfstates2"
-    storage_account_name = "tfstatepractice"
+    storage_account_name = "tfstateworkshop"
     container_name       = "tfstatecontainer"
     key                  = "prod.terraform.tfstate"
   }
