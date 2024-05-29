02a35aff165ea091c2d06fb59969564c076d9d00
juanigm <juanigm99@gmail.com>
2024-05-29 11:08:21 -0300

    configured AKS cluster in main.tf and deleted old outputs

--- main.tf
+++ main.tf
@@ -4,47 +4,13 @@ module "rg" {
   name = "ansible-rg"
 }
 
-module "vnet" {
-  source = "./modules/networking"
+module "aks" {
+  source = "./modules/AKS"
 
-  vnet-name = "vnet-ansible"
-
-  rg-name = module.rg.name
-
-  sg-name = "vnet-ansible-sg"
-
-  CIDR = ["10.0.0.0/16"]
-
-  subnets = {
-    subnet1 = {
-      name           = "subnet1"
-      address_prefix = "10.0.1.0/24"
-    }
-    subnet2 = {
-      name = "subnet2"
-      address_prefix = "10.0.2.0/24"
-    }
-  }
-}
-
-module "vm" {
-  source = "./modules/VirtualMachine"
-
-  vm_name             = "Linux-vm1"
+  name = "${random_pet.random_kubernetes_cluster_name.id}-k8s"
+  location = module.rg.location
   resource_group_name = module.rg.name
-  subnet_id           = tolist(module.vnet.subnet)[0].id
-  pub_ip_name         = "public-ip-vm-1"
-  location            = module.rg.location
-
-}
-
-module "vm2" {
-  source = "./modules/VirtualMachine"
-
-  vm_name             = "Linux-vm2"
-  resource_group_name = module.rg.name
-  subnet_id           = tolist(module.vnet.subnet)[1].id
-  pub_ip_name         = "public-ip-vm-2"
-  location            = module.rg.location
+  kubernetes_version = "1.29.4"
+  dns_prefix = "${random_pet.random_kubernetes_dns_prefix.id}-k8s"
 
 }
--- output.tf
+++
@@ -1,7 +0,0 @@
-output "vm-ip" {
-  value = module.vm.vm-ip
-}
-
-output "vm-ip-2" {
-  value = module.vm2.vm-ip
-}
