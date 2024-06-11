2956f0dffcce87f8e1a6ce7c4282142a0f3cdbfd
juanigm <juanigm99@gmail.com>
2024-06-11 10:14:38 -0300

    Added VNET module

--- main.tf
+++ main.tf
@@ -4,6 +4,26 @@ module "rg" {
   name = "ansible-rg"
 }
 
+module "vnet" {
+  source = "./modules/networking"
+
+  vnet-name = "aks-vent"
+
+  rg-name = module.rg.name
+
+  # sg-name = "aks-vnet-sg"
+
+  CIDR = ["10.224.0.0/12"]
+
+  subnets = {
+    subnet1 = {
+      name           = "aks-subnet"
+      address_prefix = "10.224.0.0/16"
+    }
+  }
+
+}
+
 module "aks" {
   source = "./modules/AKS"
 
@@ -12,5 +32,5 @@ module "aks" {
   resource_group_name = module.rg.name
   kubernetes_version = "1.29.4"
   dns_prefix = "${random_pet.random_kubernetes_dns_prefix.id}-k8s"
-
+  subnet_id = tolist(module.vnet.subnet)[0].id
 }
