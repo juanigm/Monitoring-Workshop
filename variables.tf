e5dcf06dfc6c41ff5f3811cb05e77a64eaa5df97
juanigm <juanigm99@gmail.com>
2024-06-11 10:16:23 -0300

    Added variables that are used to by resources.tf

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "services" {
+
+  type = list(object({
+    name  = string
+    port  = string
+    priority = number
+  }))
+
+  default = [{
+    name = "prometheus"
+    port = "9090"
+    priority = 500
+  }, 
+  {
+    name = "grafana"
+    port = "3000"
+    priority = 501
+  },
+  {
+    name = "myapp"
+    port = "8080"
+    priority = 502
+  },
+  {
+    name = "pushgateway"
+    port = "9091"
+    priority = 503
+  },
+  {
+    name = "alertmanager"
+    port = "9093"
+    priority = 504
+  }]
+}
+
+variable "yaml-files" {
+  type = list(string)
+  default = [ "grafana", "prometheus", "myapp", "pushgateway", "alertmanager" ]
+}
