8c5ce7a5d18474e06ac42fb9318231c50b42256a
juanigm <juanigm99@gmail.com>
2024-06-11 10:12:01 -0300

    Added vnet_subnet_id to default node poll for giving custom subnet to AKS

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -4,14 +4,13 @@ resource "azurerm_kubernetes_cluster" "aks" {
   resource_group_name = var.resource_group_name
   dns_prefix          = var.dns_prefix
   kubernetes_version  = var.kubernetes_version
-
   default_node_pool {
     name            = var.default_node_pool_name
     node_count      = var.default_node_pool_node_count
     vm_size         = var.default_node_pool_vm_size
     os_disk_size_gb = var.default_node_pool_os_disk_size_gb
+    vnet_subnet_id  = var.subnet_id
   }
-
   identity {
     type = "SystemAssigned"
   }
