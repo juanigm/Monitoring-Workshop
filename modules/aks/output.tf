2866551175ff00ee1f635ee38983f7367db77ad3
juanigm <juanigm99@gmail.com>
2024-06-11 10:12:55 -0300

    Added new variable for subnet id and ouput for your location

--- modules/aks/output.tf
+++ modules/aks/output.tf
@@ -2,9 +2,6 @@ output "node-rg" {
   value = azurerm_kubernetes_cluster.aks.node_resource_group
 }
 
-output "pip_name" {
-  value = trimspace(file("${path.root}/pip_name.txt"))
-}
-output "nsg_name" {
-  value = trimspace(file("${path.root}/nsg_name.txt"))
+output "location" {
+  value = azurerm_kubernetes_cluster.aks.location
 }
